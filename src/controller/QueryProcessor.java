eb1b8d61dcaef77045f6b8a43638d1ef2197d5d5
Johannes Haeussler <Johannes.3.Haeussler@uni-konstanz.de>
2016-06-24 17:47:30 +0200

    use multithreading

--- src/controller/QueryProcessor.java
+++ src/controller/QueryProcessor.java
@@ -10,10 +10,12 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Observable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import javax.imageio.ImageIO;
 
@@ -57,46 +59,67 @@ public class QueryProcessor extends Observable {
       IFeature featureQueryImage =   extractor.process(model.getQueryImage()).setMeasure(measure);
       
       // For testing
-      List<ResultImage> results = new ArrayList<ResultImage> ();
+      final List<ResultImage> results = new ArrayList<ResultImage> ();
       //String[] resL = model.getQueryImageFile().getParentFile().list();
       
       File rootImage = model.getQueryImageFile().getParentFile().getParentFile();
       ArrayList<File> images = new ArrayList<>();
       listf(rootImage , images);
       
+      ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+      
       for(int i = 0; i < images.size(); i++) {
          File f =  images.get(i);// resL[i];
-         try {
-            ResultImage res = new ResultImage();
-            BufferedImage resI = ImageIO.read(f);
-            //System.out.println(f.getAbsolutePath());
-            
-            
-            //calculateSimilarity(featureQueryImage);
-            //res.setSimilarity( measure.calculateSimilarity(featureQueryImage, featureCandidate));
-            
-            res.setSimilarity(featureQueryImage.calculateSimilarity(extractor.process(resI)));
-           // res.setSimilarity(0);
-            res.setFile(f);
-            //res.setImage(resI);
-            res.setThumbnail(ImageController.getInstance().scaleImage(resI, 100));
-            res.setRank(0);
-            res.setCategory(f.getParentFile().getName());
-            results.add(res);
-         } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-         }
+         executor.submit(() -> {
+            try {
+               ResultImage res = new ResultImage();
+               BufferedImage resI = ImageIO.read(f);
+               //System.out.println(f.getAbsolutePath());
+               
+               
+               //calculateSimilarity(featureQueryImage);
+               //res.setSimilarity( measure.calculateSimilarity(featureQueryImage, featureCandidate));
+               
+               res.setSimilarity(featureQueryImage.calculateSimilarity(extractor.process(resI)));
+              // res.setSimilarity(0);
+               res.setFile(f);
+               //res.setImage(resI);
+               res.setThumbnail(ImageController.getInstance().scaleImage(resI, 100));
+               res.setRank(0);
+               res.setCategory(f.getParentFile().getName());
+               results.add(res);
+            } catch (IOException e) {
+               // TODO Auto-generated catch block
+               e.printStackTrace();
+            }
+        });
       }
+      
+      try {
+         System.out.println("attempt to shutdown executor");
+         executor.shutdown();
+         executor.awaitTermination(5, TimeUnit.MINUTES);
+     }
+     catch (InterruptedException e) {
+         System.err.println("tasks interrupted");
+     }
+     finally {
+         if (!executor.isTerminated()) {
+             System.err.println("cancel non-finished tasks");
+         }
+         executor.shutdownNow();
+         System.out.println("shutdown finished");
+     }
       
       // sort the results according to the distance
       Collections.sort(results, (a, b) ->  b.compareTo(a) );
       
-      // update rank label
+      List<ResultImage> topResults = results.subList(0, 499);
       
-      model.setResultImages(results);
-      for (int i = 0; i < results.size(); i++) {
-    	  ((ResultImage) results.get(i)).setRank(i+1);
+      // update rank label
+      model.setResultImages(topResults);
+      for (int i = 0; i < topResults.size(); i++) {
+    	  ((ResultImage) topResults.get(i)).setRank(i+1);
       }
       
       
